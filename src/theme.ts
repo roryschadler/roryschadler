b8d6a09f89bd5f3dcf56c78ed8ea9502e330c4c5
Rory Schadler <48921090+roryschadler@users.noreply.github.com>
2023-11-21 17:02:10 -0500

    Remove unnecessary fonts

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,8 @@
 import { createTheme, PaletteMode, responsiveFontSizes } from '@mui/material';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/latin-300.css';
+import '@fontsource/roboto/latin-400.css';
+import '@fontsource/roboto/latin-500.css';
+import '@fontsource/roboto/latin-700.css';
 
 const getTheme = (mode: PaletteMode) =>
   responsiveFontSizes(
