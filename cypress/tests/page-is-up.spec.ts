d09adce73600b978056b86db9ddca5ca8e6985ce
Rory Schadler <48921090+roryschadler@users.noreply.github.com>
2023-11-25 13:46:25 -0500

    Public repo actions are free, run all tests daily
    No need to only run a quick test daily, we can do them all

--- cypress/tests/page-is-up.spec.ts
+++
@@ -1,7 +0,0 @@
-// leaving this short as a quick e2e test that can run daily
-describe('homepage', () => {
-  it('should render the homepage', () => {
-    cy.visit('/');
-    cy.get('h1').should('contain', 'Rory Schadler');
-  });
-});
